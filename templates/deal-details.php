48ddc57af253f4b2494e19528589dcd7a524b1aa
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-02-26 11:31:38 +0100

    Convert Bootstrap panels to cards.

--- templates/deal-details.php
+++ templates/deal-details.php
@@ -7,27 +7,28 @@ $company_id = get_post_meta( $post->ID, '_orbis_deal_company_id', true );
 $company_post_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM $wpdb->orbis_companies WHERE id = %d;", $company_id ) );
 
 ?>
-<div class="panel">
-	<header>
-		<h3><?php esc_html_e( 'Deal Details', 'orbis_deals' ); ?></h3>
-	</header>
+<div class="card mb-3">
+	<div class="card-body">
+		<h3 class="card-title"><?php esc_html_e( 'Deal Details', 'orbis_deals' ); ?></h3>
 
-	<div class="content">
-		<dl>
-			<dt><?php esc_html_e( 'Company', 'orbis_deals' ); ?></dt>
-			<dd>
-				<a href="<?php echo esc_attr( get_permalink( $company_post_id ) ); ?>"><?php orbis_deal_the_company_name(); ?></a>
-			</dd>
+		<div class="content">
+			<dl>
+				<dt><?php esc_html_e( 'Company', 'orbis_deals' ); ?></dt>
+				<dd>
+					<a href="<?php echo esc_attr( get_permalink( $company_post_id ) ); ?>"><?php orbis_deal_the_company_name(); ?></a>
+				</dd>
 
-			<dt><?php esc_html_e( 'Price', 'orbis_deals' ); ?></dt>
-			<dd>
-				<?php orbis_deal_the_price(); ?>
-			</dd>
+				<dt><?php esc_html_e( 'Price', 'orbis_deals' ); ?></dt>
+				<dd>
+					<?php orbis_deal_the_price(); ?>
+				</dd>
 
-			<dt><?php esc_html_e( 'Status', 'orbis_deals' ); ?></dt>
-			<dd>
-				<?php orbis_deal_the_status(); ?>
-			</dd>
-		</dl>
+				<dt><?php esc_html_e( 'Status', 'orbis_deals' ); ?></dt>
+				<dd>
+					<?php orbis_deal_the_status(); ?>
+				</dd>
+			</dl>
+		</div>
 	</div>
+	
 </div>
