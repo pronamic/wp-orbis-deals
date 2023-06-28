adff1491081de9e2951fbede351d34ea392440a0
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-06-28 17:27:43 +0200

    Update rest.php

--- includes/rest.php
+++ includes/rest.php
@@ -95,12 +95,75 @@ class Orbis_Deals_RestController {
 	public function rest_api_get( WP_REST_Request $request ) {
 		$post_id = $request->get_param( 'post_id' );
 
+		$customer = [
+			'first_name'    => '',
+			'last_name'     => '',
+			'company_name'  => '',
+			'kvk_number'    => '',
+			'vat_number'    => '',
+			'address'       => '',
+			'postal_code'   => '',
+			'city'          => '',
+			'invoice_email' => '',
+		];
+
+		// Companies.
+		$companies = \get_posts(
+			[
+				'post_type'        => 'orbis_company',
+				'connected_type'   => 'orbis_deals_to_companies',
+				'connected_items'  => $post_id,
+				'posts_per_page'   => 1,
+				'suppress_filters' => false,
+			]
+		);
+
+		$company = \reset( $companies );
+
+		// Persons.
+		$persons = \get_posts(
+			[
+				'post_type'        => 'orbis_person',
+				'connected_type'   => 'orbis_deals_to_persons',
+				'connected_items'  => $post_id,
+				'posts_per_page'   => 1,
+				'suppress_filters' => false,
+			]
+		);
+
+		$person = \reset( $persons );
+
+		// Data.
+		if ( false !== $company ) {
+			$customer['company_name']  = get_the_title( $company );
+			$customer['kvk_number']    = get_post_meta( $company->ID, '_orbis_kvk_number', true );
+			$customer['vat_number']    = get_post_meta( $company->ID, '_orbis_vat_number', true );
+			$customer['address']       = get_post_meta( $company->ID, '_orbis_address', true );
+			$customer['postal_code']   = get_post_meta( $company->ID, '_orbis_postcode', true );
+			$customer['city']          = get_post_meta( $company->ID, '_orbis_city', true );
+			$customer['invoice_email'] = get_post_meta( $company->ID, '_orbis_invoice_email', true );
+		}
+
+		if ( false !== $person ) {
+			$names = explode( ' ', get_the_title( $person ) );
+
+			$first_name = array_shift( $names );
+
+			$customer['first_name'] = $first_name;
+			$customer['last_name']  = implode( ' ', $names );
+		}
+
 		$lines = (array) json_decode( get_post_meta( $post_id, '_orbis_deal_lines', true ) );
 
 		return [
-			'post_id' => $post_id,
-			'hash'    => \wp_hash( $post_id ),
-			'lines'   => $lines,
+			'post_id'  => $post_id,
+			'hash'     => \wp_hash( $post_id ),
+			'customer' => $customer,
+			'supplier' => [
+				'name'  => get_the_author_meta( 'display_name', get_post_field( 'post_author', $post_id ) ),
+				'email' => get_the_author_meta( 'user_email', get_post_field( 'post_author', $post_id ) ),
+			],
+			'lines'    => $lines,
 		];
 	}
 
